86b38abd6fe7966f5ed128239c451a5f50c52234
rusyajkeee <230432@astanait.edu.kz>
2025-02-20 13:47:35 +0500

    new link for api!!

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://foodik.onrender.com/"
+    const url = "https://foodik.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
